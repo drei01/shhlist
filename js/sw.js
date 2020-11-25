5e01a12a93d8e1fee07bd888c63a597536f1657e
drei01 <drei01@users.noreply.github.com>
2020-11-25 08:30:25 +0000

    deploy: 56cedcac9325d15102241a57caab1b73e90ede34

--- js/sw.js
+++ js/sw.js
@@ -1,6 +1,4 @@
 self.addEventListener('fetch', (event) => {
-    console.log(`Request of ${event.request.url}`);
-
     // default behaviour: request the network
     event.respondWith(fetch(event.request));
 });
